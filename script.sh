ca877c2322eb82c9a3ef6096b3ea935a4206e43e
nicknase27 <80787938+nicknase27@users.noreply.github.com>
2021-08-15 16:18:27 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+sudo pacman -S base-devel --noconfirm
+
+sudo pacman -Sy --noconfirm
+
+sudo pacman -Syu --noconfirm
+
 sudo pacman -S nvidia-dkms nvidia-utils lib32-nvidia-utils nvidia-settings vulkan --noconfirm
 
 sudo pacman -S wine-staging giflib lib32-giflib libpng lib32-libpng libldap lib32 --noconfirm
