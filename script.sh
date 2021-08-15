19b52726d3741c4061917068de73b1388e8b6785
nicknase27 <80787938+nicknase27@users.noreply.github.com>
2021-08-15 16:15:22 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 
-sudo pacman -S nvidia-dkms nvidia-utils lib32-nvidia-utils nvidia-settings vulkan
+sudo pacman -S nvidia-dkms nvidia-utils lib32-nvidia-utils nvidia-settings vulkan --noconfirm
 
-sudo pacman -S wine-staging giflib lib32-giflib libpng lib32-libpng libldap lib32
+sudo pacman -S wine-staging giflib lib32-giflib libpng lib32-libpng libldap lib32 --noconfirm
 
-sudo pacman -S gamemode lib32-gamemode
+sudo pacman -S gamemode lib32-gamemode --noconfirm
 
-sudo pacman -S steam lutris
+sudo pacman -S steam lutris --noconfirm
 
-sudo pacman -S linux-zen linux-zen-headers
+sudo pacman -S linux-lts linux-lts-headers --noconfirm
 
 git clone https://github.com/Jguer/yay.git
 
 cd yay
 
-makepkg -si
+makepkg -si --noconfirm
