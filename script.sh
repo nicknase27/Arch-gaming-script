87fc50dad80fa2eabd05681ab3bba9305db0fca1
nicknase27 <nicknasekatschank@gmail.com>
2021-11-05 19:13:46 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -10,11 +10,11 @@ sudo pacman -S nvidia-dkms nvidia-utils lib32-nvidia-utils nvidia-settings vulka
 
 sudo pacman -S wine-staging giflib lib32-giflib libpng lib32-libpng libldap lib32 --noconfirm
 
+sudo pacman -S flameshot i3-gaps neofetch --noconfirm
+
 sudo pacman -S gamemode lib32-gamemode --noconfirm
 
-sudo pacman -S steam lutris --noconfirm
-
-sudo pacman -S linux-lts linux-lts-headers --noconfirm
+sudo pacman -S steam lutris discord-canary ntfs-3g --noconfirm
 
 git clone https://github.com/Jguer/yay.git
 
@@ -22,4 +22,6 @@ cd yay
 
 makepkg -si --noconfirm
 
-yay -S spotify multimc5 --noconfirm
+yay -S spotify-adblock multimc5  oh-my-zsh-git freetube grapejuice-git cava unimatrix --noconfirm
+
+wget https://raw.githubusercontent.com/endeavouros-team/endeavouros-i3wm-setup/main/setup.sh
