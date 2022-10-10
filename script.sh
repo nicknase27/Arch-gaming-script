a8302a5f1865f6427da5384c163107321430edbb
nicknase27 <80787938+nicknase27@users.noreply.github.com>
2022-10-10 13:25:31 +0200

    updoot

--- script.sh
+++ script.sh
@@ -1,33 +1,34 @@
-#!/bin/sh
+cd /home/nick/
 
-sudo pacman -S base-devel --noconfirm
+sudo pacman -Syu
 
-sudo pacman -Sy --noconfirm
 
-sudo pacman -Syu --noconfirm
+sudo mkdir Documents Downloads Picutres Movies Music
+sudo chown nick:nick Documents Downloads Picutres Movies Music
 
-sudo pacman -S nvidia-dkms nvidia-utils lib32-nvidia-utils nvidia-settings vulkan --noconfirm
-
-sudo pacman -S wine-staging giflib lib32-giflib libpng lib32-libpng libldap lib32 --noconfirm
-
-sudo pacman -S flameshot i3-gaps neofetch  zsh --noconfirm
-
-sudo pacman -S gamemode lib32-gamemode --noconfirm
-
-sudo pacman -S steam lutris discord-canary --noconfirm
-
+mkdir git && cd git 
 git clone https://github.com/Jguer/yay.git
-
 cd yay
+makepkg -si
+cd
 
-makepkg -si --noconfirm
+sudo pacman -S alacritty rofi dmenu discord dunst fish nvidia nvidia-settings flameshot picom nitrogen neovim pfetch compsize polybar bitwarden
+yay -S brave-bin spotify-adblock
 
-yay -S spotify-adblock multimc5  oh-my-zsh-git freetube grapejuice-git cava unimatrix --noconfirm
 
-wget https://raw.githubusercontent.com/endeavouros-team/endeavouros-i3wm-setup/main/setup.sh
+cd git
+git clone https://github.com/nicknase27/dotfiles-new.git
+cd dotfiles-new
+cp -r bspwm/ ~/.config && chmod +x bspwmrc
+cp -r sxhkd/ ~/.config && chmod +x sxhkdrc
+cp -r alacritty/ ~/.config
+cp -r fish/ ~/.config
+cp -r dunst/ ~/.config
+cp -r polybar/ ~/.config
+cp -r rofi/ ~/.config
+cd
 
-git clone https://github.com/Chick2D/neofetch-themes.git
+curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
+omf install lambda
 
-cd neofetch-themes-main/small
-
-sudo mv blockfetch.conf /.config/neofetch
+pfetch
